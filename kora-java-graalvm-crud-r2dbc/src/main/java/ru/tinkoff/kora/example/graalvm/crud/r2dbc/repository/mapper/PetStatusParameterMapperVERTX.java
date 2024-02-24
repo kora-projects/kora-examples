d17f3b5c939b4c1800ae8153ce23974886250b1a
Anton Kurako <goodforgod.dev@gmail.com>
2024-02-25 01:00:09 +0300

    Kora Java GraalVM CRUD R2DBC example added

---
+++ kora-java-graalvm-crud-r2dbc/src/main/java/ru/tinkoff/kora/example/graalvm/crud/r2dbc/repository/mapper/PetStatusParameterMapperVERTX.java
@@ -0,0 +1,18 @@
+package ru.tinkoff.kora.example.graalvm.crud.r2dbc.repository.mapper;
+
+import ru.tinkoff.kora.common.Component;
+
+@Component
+public final class PetStatusParameterMapperVERTX
+// implements VertxParameterColumnMapper<Pet.Status>
+{
+
+    // @Override
+    // public Object apply(@Nullable Pet.Status value) {
+    // if (value == null) {
+    // return null;
+    // } else {
+    // return value.code;
+    // }
+    // }
+}
