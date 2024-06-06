692137d2d87ef9d0220ba97630e51b382a0c704d
Anton Kurako <goodforgod.dev@gmail.com>
2024-06-06 14:21:54 +0300

    Camunda 7 tests added

--- kora-java-camunda-engine/src/main/java/ru/tinkoff/kora/example/camunda/engine/Application.java
+++ kora-java-camunda-engine/src/main/java/ru/tinkoff/kora/example/camunda/engine/Application.java
@@ -8,13 +8,14 @@ import ru.tinkoff.kora.config.hocon.HoconConfigModule;
 import ru.tinkoff.kora.database.jdbc.JdbcDatabaseModule;
 import ru.tinkoff.kora.http.server.undertow.UndertowHttpServerModule;
 import ru.tinkoff.kora.json.common.JsonCommonModule;
+import ru.tinkoff.kora.json.module.JsonModule;
 import ru.tinkoff.kora.logging.logback.LogbackModule;
 
 @KoraApp
 public interface Application extends
         HoconConfigModule,
         LogbackModule,
-        JsonCommonModule,
+        JsonModule,
         UndertowHttpServerModule,
         CamundaEngineModule,
         CamundaRestUndertowModule,
--- kora-java-camunda-engine/src/main/java/ru/tinkoff/kora/example/camunda/engine/CamundaController.java
+++ kora-java-camunda-engine/src/main/java/ru/tinkoff/kora/example/camunda/engine/CamundaController.java
@@ -1,17 +1,25 @@
 package ru.tinkoff.kora.example.camunda.engine;
 
-import java.util.stream.Collectors;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.repository.ResourceDefinition;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import ru.tinkoff.kora.common.Component;
 import ru.tinkoff.kora.http.common.HttpMethod;
 import ru.tinkoff.kora.http.common.HttpResponseEntity;
 import ru.tinkoff.kora.http.common.annotation.HttpRoute;
 import ru.tinkoff.kora.http.server.common.annotation.HttpController;
+import ru.tinkoff.kora.json.common.annotation.Json;
+
+import java.util.List;
+import java.util.UUID;
 
 @Component
 @HttpController("/camunda")
 public final class CamundaController {
+
+    @Json
+    public record CamundaProcess(String instanceId, String businessKey) {
+    }
 
     private final ProcessEngine processEngine;
 
@@ -25,15 +33,26 @@ public final class CamundaController {
     }
 
     @HttpRoute(method = HttpMethod.GET, path = "/definitions")
-    public HttpResponseEntity<String> definitions() {
+    public HttpResponseEntity<List<String>> definitions() {
         return HttpResponseEntity.of(200, processEngine.getRepositoryService().createProcessDefinitionQuery().list().stream()
                 .map(ResourceDefinition::getKey)
                 .sorted()
-                .collect(Collectors.joining(",")));
+                .toList());
     }
 
-    @HttpRoute(method = HttpMethod.GET, path = "/start")
-    public HttpResponseEntity<String> startHelloWorldProcess() {
-        return HttpResponseEntity.of(200, processEngine.getRuntimeService().startProcessInstanceByKey("HelloWorld").getId());
+    @Json
+    @HttpRoute(method = HttpMethod.GET, path = "/start/helloworld")
+    public HttpResponseEntity<CamundaProcess> startHelloWorldProcess() {
+        String businessKey = UUID.randomUUID().toString();
+        ProcessInstance instance = processEngine.getRuntimeService().startProcessInstanceByKey("HelloWorld", businessKey);
+        return HttpResponseEntity.of(200, new CamundaProcess(instance.getId(), businessKey));
+    }
+
+    @Json
+    @HttpRoute(method = HttpMethod.GET, path = "/start/onboarding")
+    public HttpResponseEntity<CamundaProcess> startOnboarding() {
+        String businessKey = UUID.randomUUID().toString();
+        ProcessInstance instance = processEngine.getRuntimeService().startProcessInstanceByKey("Onboarding", businessKey);
+        return HttpResponseEntity.of(200, new CamundaProcess(instance.getId(), businessKey));
     }
 }
