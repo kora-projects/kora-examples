d45326c7b32ec5f0530ccb904cfb430fa5ad2438
Anton Kurako <goodforgod.dev@gmail.com>
2024-10-29 12:33:18 +0300

    Cleanup

--- kora-java-kafka/src/main/java/ru/tinkoff/kora/example/kafka/publisher/TopicKeyHeadersPublisher.java
+++ kora-java-kafka/src/main/java/ru/tinkoff/kora/example/kafka/publisher/TopicKeyHeadersPublisher.java
@@ -2,10 +2,11 @@ package ru.tinkoff.kora.example.kafka.publisher;
 
 import org.apache.kafka.common.header.Headers;
 import ru.tinkoff.kora.kafka.common.annotation.KafkaPublisher;
+import ru.tinkoff.kora.kafka.common.annotation.KafkaPublisher.Topic;
 
 @KafkaPublisher("kafka.producer.my-publisher")
 public interface TopicKeyHeadersPublisher {
 
-    @KafkaPublisher.Topic("kafka.producer.my-topic")
+    @Topic("kafka.producer.my-topic")
     void send(String key, String value, Headers headers);
 }
--- kora-java-kafka/src/main/java/ru/tinkoff/kora/example/kafka/publisher/TopicPublisher.java
+++ kora-java-kafka/src/main/java/ru/tinkoff/kora/example/kafka/publisher/TopicPublisher.java
@@ -3,16 +3,17 @@ package ru.tinkoff.kora.example.kafka.publisher;
 import java.util.concurrent.Future;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import ru.tinkoff.kora.kafka.common.annotation.KafkaPublisher;
+import ru.tinkoff.kora.kafka.common.annotation.KafkaPublisher.Topic;
 
 @KafkaPublisher("kafka.producer.my-publisher")
 public interface TopicPublisher {
 
-    @KafkaPublisher.Topic("kafka.producer.my-topic")
+    @Topic("kafka.producer.my-topic")
     void send(String value);
 
-    @KafkaPublisher.Topic("kafka.producer.my-topic")
+    @Topic("kafka.producer.my-topic")
     RecordMetadata sendMeta(String value);
 
-    @KafkaPublisher.Topic("kafka.producer.my-topic")
+    @Topic("kafka.producer.my-topic")
     Future<RecordMetadata> sendMetaAsync(String value);
 }
