a964dad634b2239f309a221e16e4faf8d41588b0
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2025-08-19 09:39:03 +0300

    1.2.0 (#33)
    * 1.2.0

--- kora-kotlin-crud/src/main/kotlin/ru/tinkoff/kora/kotlin/example/crud/model/PetMapper.kt
+++ kora-kotlin-crud/src/main/kotlin/ru/tinkoff/kora/kotlin/example/crud/model/PetMapper.kt
@@ -1,14 +1,30 @@
 package ru.tinkoff.kora.kotlin.example.crud.model
 
-import org.mapstruct.Mapper
-import org.mapstruct.Mapping
+import ru.tinkoff.kora.common.Component
 import ru.tinkoff.kora.example.crud.openapi.http.server.model.CategoryTO
 import ru.tinkoff.kora.example.crud.openapi.http.server.model.PetTO
+import ru.tinkoff.kora.example.crud.openapi.http.server.model.PetTO.StatusEnum
 
-@Mapper
 interface PetMapper {
 
     fun petWithCategoryToPetTO(pet: PetWithCategory): PetTO
 
     fun petCategoryToCategoryTO(category: PetCategory): CategoryTO
+}
+
+@Component
+class ManualPetMapper : PetMapper {
+
+    override fun petWithCategoryToPetTO(pet: PetWithCategory): PetTO {
+        val enum: StatusEnum = when (pet.status) {
+            Pet.Status.AVAILABLE -> StatusEnum.AVAILABLE
+            Pet.Status.PENDING -> StatusEnum.PENDING
+            Pet.Status.SOLD -> StatusEnum.SOLD
+        }
+        return PetTO(pet.id, pet.name, enum, petCategoryToCategoryTO(pet.category))
+    }
+
+    override fun petCategoryToCategoryTO(category: PetCategory): CategoryTO {
+        return CategoryTO(category.id, category.name)
+    }
 }
