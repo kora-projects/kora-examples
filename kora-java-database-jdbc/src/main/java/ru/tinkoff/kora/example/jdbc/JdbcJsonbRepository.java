60349ee40498b20042aa56ae9512fb74ea627a2e
ᴀɴᴛᴏɴ <GoodforGod@users.noreply.github.com>
2025-10-08 11:51:41 +0300

    1.2.4 (#37)
    * 1.2.4

--- kora-java-database-jdbc/src/main/java/ru/tinkoff/kora/example/jdbc/JdbcJsonbRepository.java
+++ kora-java-database-jdbc/src/main/java/ru/tinkoff/kora/example/jdbc/JdbcJsonbRepository.java
@@ -1,71 +1,23 @@
 package ru.tinkoff.kora.example.jdbc;
 
 import jakarta.annotation.Nullable;
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.UUID;
-import org.postgresql.util.PGobject;
-import ru.tinkoff.kora.common.Mapping;
 import ru.tinkoff.kora.database.common.annotation.Column;
 import ru.tinkoff.kora.database.common.annotation.Query;
 import ru.tinkoff.kora.database.common.annotation.Repository;
 import ru.tinkoff.kora.database.jdbc.EntityJdbc;
 import ru.tinkoff.kora.database.jdbc.JdbcRepository;
-import ru.tinkoff.kora.database.jdbc.mapper.parameter.JdbcParameterColumnMapper;
-import ru.tinkoff.kora.database.jdbc.mapper.result.JdbcResultColumnMapper;
-import ru.tinkoff.kora.json.common.JsonReader;
-import ru.tinkoff.kora.json.common.JsonWriter;
 import ru.tinkoff.kora.json.common.annotation.Json;
 
+/**
+ * @see JdbcJsonbMapperModule
+ */
 @Repository
 public interface JdbcJsonbRepository extends JdbcRepository {
 
-    final class JsonbResultMapper implements JdbcResultColumnMapper<Entity.JsonbValue> {
-
-        private final JsonReader<Entity.JsonbValue> reader;
-
-        public JsonbResultMapper(JsonReader<Entity.JsonbValue> reader) {
-            this.reader = reader;
-        }
-
-        @Override
-        public Entity.JsonbValue apply(ResultSet rs, int index) throws SQLException {
-            try {
-                var value = rs.getString(index);
-                return reader.read(value);
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-    }
-
-    final class JsonbParameterMapper implements JdbcParameterColumnMapper<Entity.JsonbValue> {
-
-        private final JsonWriter<Entity.JsonbValue> writer;
-
-        public JsonbParameterMapper(JsonWriter<Entity.JsonbValue> writer) {
-            this.writer = writer;
-        }
-
-        @Override
-        public void set(PreparedStatement stmt, int index, @Nullable Entity.JsonbValue value) throws SQLException {
-            if (value != null) {
-                PGobject jsonb = new PGobject();
-                jsonb.setType("jsonb");
-                jsonb.setValue(writer.toStringUnchecked(value));
-                stmt.setObject(index, jsonb);
-            }
-        }
-    }
-
     @EntityJdbc
     record Entity(UUID id,
-                  @Mapping(JsonbResultMapper.class)
-                  @Mapping(JsonbParameterMapper.class)
-                  @Column("value") JsonbValue value) {
+                  @Column("value") @Json JsonbValue value) {
 
         @Json
         public record JsonbValue(String name, String surname) {}
@@ -77,7 +29,7 @@ public interface JdbcJsonbRepository extends JdbcRepository {
 
     @Query("""
             INSERT INTO entities_jsonb(id, value)
-            VALUES (:entity.id, :entity.value)
+            VALUES (:entity.id, :entity.value::jsonb)
             """)
     void insert(Entity entity);
 }
