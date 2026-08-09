9549a61a0f50f723204f30f4d0c202c503ca7d0d
Anton Kurako <goodforgod.dev@gmail.com>
2026-08-10 00:26:43 +0300

    PROMPT.md

--- examples/java/kora-java-config-hocon/src/main/java/io/koraframework/example/config/hocon/Application.java
+++ examples/java/kora-java-config-hocon/src/main/java/io/koraframework/example/config/hocon/Application.java
@@ -11,6 +11,6 @@ public interface Application extends
         LogbackModule {
 
     static void main(String[] args) {
-        KoraApplication.run(ApplicationGraph::graph);
+        KoraApplication.run(ApplicationGraph::graph, true);
     }
 }
--- examples/java/kora-java-config-hocon/src/main/java/io/koraframework/example/config/hocon/RootService.java
+++ examples/java/kora-java-config-hocon/src/main/java/io/koraframework/example/config/hocon/RootService.java
@@ -1,15 +1,38 @@
 package io.koraframework.example.config.hocon;
 
+import io.koraframework.application.graph.Lifecycle;
 import io.koraframework.common.annotation.Component;
 import io.koraframework.common.annotation.Root;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.UncheckedIOException;
 
 @Root
 @Component
-public final class RootService {
+public final class RootService implements Lifecycle {
 
+    private static final Logger log = LoggerFactory.getLogger(RootService.class);
     private final FooConfig fooConfig;
 
     public RootService(FooConfig fooConfig) {
         this.fooConfig = fooConfig;
+    }
+
+    @Override
+    public void init() throws Exception {
+        log.info("INIT");
+        try {
+            if(true) {
+                throw new IllegalStateException("OPS");
+            }
+        } catch (IllegalStateException e) {
+            // do nothing
+        }
+    }
+
+    @Override
+    public void release() throws Exception {
+        log.info("RELEASE");
     }
 }
